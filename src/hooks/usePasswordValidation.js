d97146ab67fa1194cd9282af01c3a83c5178c9fd
Abhishek Kumar Yadav <45727674+coderaky@users.noreply.github.com>
2022-03-25 16:53:23 +0530

    Update usePasswordValidation.js

--- src/hooks/usePasswordValidation.js
+++ src/hooks/usePasswordValidation.js
@@ -26,37 +26,3 @@ setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(pass.charAt(username.
   
 return [validLength, hasNumber, validlen, match, specialChar];
 };
-
-
-
-
-
-
-
-
-
-// import { useState, useEffect } from "react";
-
-// export const usePasswordValidation = ({
-  
-// username = "",
-// password = "",
-// requiredLength = 8,
-  
-// }) => {
-// const [validLength, setValidLength] = useState(null);
-// const [hasNumber, setHasNumber] = useState(null);
-// const [specialChar, setSpecialChar] = useState(null);
-// const [match, setMatch] = useState(null);
-  
-// useEffect(() => {
-  
-// setValidLength(username.length <= 8 ? true : false && password.length <= 12 ? true : false);
-// setHasNumber(/\d/.test(password.charAt(password.length - 1)) && /\d/.test(password.charAt(password.length - 2))  && /\d/.test(password.charAt(password.length - 3)));
-// setMatch(username && username === password.substring(0, username.length));
-// setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(password.charAt(password.length - 4)));
-  
-// }, [username, password, requiredLength]);
-  
-// return [validLength, hasNumber, match, specialChar];
-// };
